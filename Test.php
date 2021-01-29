ab9d4295f9e42ee328ccc98c5a05b95fe78af2d3
Sergio Alonso <sergio.alonso@absolute-design.co.uk>
2021-01-29 16:32:08 +0000

    Adding Registration

---
+++ Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Saaideveloper\Framework;
+
+class Test  
+{ 
+
+    public function __construct() {
+
+    }
+
+    public function test(){
+        return "SimpleClass::test";
+    }
+}
