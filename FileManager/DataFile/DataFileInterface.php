7b3ad15379de86b06d3e4254d6d901e9d8aadfe9
Sergio Alonso <saaideveloper@gmail.com>
2021-01-30 16:10:43 +0000

    Bug missing

--- FileManager/DataFile/DataFileInterface.php
+++ FileManager/DataFile/DataFileInterface.php
@@ -18,4 +18,38 @@ interface DataFileInterface
      */
     public function test();
 
+    /**
+     * @TODO : Check Params 
+     * 
+     * @see Magento/Framework/Filesystem/IO/File :: streamRead
+     */
+    public function streamRead();
+
+    /**
+     * @TODO : Check Params 
+     * 
+     * @see Magento/Framework/Filesystem/IO/File :: streamWrite
+     */
+    public function streamWrite();
+
+    /**
+     * @TODO : Check Params 
+     * 
+     * @see Magento/Framework/Filesystem/IO/File :: streamClose
+     */
+    public function streamClose();
+
+    /**
+     * @TODO : Check Params 
+     * 
+     * @see Magento/Framework/Filesystem/IO/File :: fileExists
+     */
+    public function iswriteable ();
+
+    /**
+     * @TODO : Check Params 
+     * 
+     * @see Magento/Framework/Filesystem/IO/File :: getPathInfo
+     */
+    public function getPathInfo();
 }
