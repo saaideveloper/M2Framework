b7e398ad5988ffa23695282da37d451cada9a43b
Sergio Alonso <saaideveloper@gmail.com>
2021-01-30 21:46:32 +0000

    Adding Types of Products Files

---
+++ FileManager/DataFile/Csv/Product/ConfigurableProductCsv.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Sergio Abad Alonso <saaideveloper@gmail.com>
+ */
+namespace Saaideveloper\Framework\FileManager\DataFile\Csv\Product;
+
+
+class ConfigurableProductCsv extends ProductCsv
+{
+    /**
+     * @var  string
+     */
+    public $_test= 'test';
+
+    public function __construct() {
+        //parent::__construct();
+    }
+
+    public function test(){
+        return "Saaideveloper_Framework_Filemanager_DataFile_Csv_Product_ConfigurableProductCsv::test";
+    }
+
+}
--- FileManager/DataFile/Csv/Product/ProductCsv.php
+++ FileManager/DataFile/Csv/Product/ProductCsv.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @author Sergio Abad <saaideveloper@gmail.com>
+ * @see https://docs.magento.com/user-guide/system/data-attributes-product.html
+ * @see https://www.mageplaza.com/devdocs/magento-2-create-product-programmatically.html
+ */
 namespace Saaideveloper\Framework\FileManager\DataFile\Csv\Product;
 
 use Saaideveloper\Framework\FileManager\DataFile\Csv\AbstractDataFileCsv;
@@ -10,7 +14,10 @@ use Saaideveloper\Framework\FileManager\DataFile\Csv\AbstractDataFileCsv;
  */
 class ProductCsv extends AbstractDataFileCsv
 {
-   
+    /**
+     * @var  string
+     */
+    public $_test= 'test';
 
     public function __construct() {
         //parent::__construct();
@@ -26,42 +33,42 @@ class ProductCsv extends AbstractDataFileCsv
      * @TODO : Finish logic generateCsvLine
      * @return string
      */
-    public function generateCsvLine(){return "generateCsvLine";}
+    //public function generateCsvLine(){return "generateCsvLine";}
 
     /**
      * Generate Simple product
      * @TODO : Finish Logic generateSimpleProduct
      * @return string 
      */
-    public function generateSimpleProduct(){ return"generateSimpleProduct";}
+    //public function generateSimpleProduct(){ return"generateSimpleProduct";}
 
     /**
      * Generate Configurable product
      * @TODO : Finish Logic generateConfigurableProduct
      * @return string
      */
-    public function generateConfigurableProduct(){ return"generateConfigurableProduct";}
+    //public function generateConfigurableProduct(){ return"generateConfigurableProduct";}
       
     /**
      * Generate Group Product
      * @TODO : Finish Logic generateGroupProduct
      * @return string
      */
-    public function generateGroupProduct(){ return"generateGroupProduct";}
+    //public function generateGroupProduct(){ return"generateGroupProduct";}
 
     /**
      * Generate Bundle Product
      * @TODO : Finish Logic generateBundleProduct
      * @return string
      */
-    public function generateBundleProduct(){ return"generateBundleProduct";}
+    //public function generateBundleProduct(){ return"generateBundleProduct";}
     
     /**
      * Generate Downloadable Product
      * @TODO : Finish Logic generateDownloadableProduct
      * @return string
      */
-    public function generateDownloadableProduct(){ return"generateDownloadableProduct";}
+    //public function generateDownloadableProduct(){ return"generateDownloadableProduct";}
 
 
 }
